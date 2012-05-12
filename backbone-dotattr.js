e7611bae182ae37cd753bb620e23a5b540b092c0
Thomas Chandler <zohogorganzola@gmail.com>
2012-05-11 23:56:15 -0400

    Added the ability to get an array of attributes at a given level using a wildcard selector '*'

--- backbone-dotattr.js
+++ backbone-dotattr.js
@@ -1,11 +1,15 @@
 (function(_, Backbone) {
     _.extend(Backbone.Model.prototype, {
         get: function(key) {
-            return _.reduce(key.split('.'), function(attr, key) {
-                if (attr instanceof Backbone.Model)
-                    return attr.attributes[key];
-
-                return attr[key];
+           return _.reduce(key.split('.'), function(attr, key) {
+                if(key==='*') {
+					if(attr instanceof Backbone.Model) attr = attr.attributes;
+					return _.filter(attr, function(val, key, obj) {
+						return (obj instanceof Backbone.Model) ? obj.attributes[key] : obj[key];
+					});
+                } else {
+					return (attr instanceof Backbone.Model) ? attr.attributes[key] : attr[key];
+                }
             }, this.attributes);
         }
     });
--- test/tests.js
+++ test/tests.js
@@ -13,3 +13,17 @@ test("get model attribute", function() {
     equal(song.get('album.title'), "Sgt. Pepper's Lonely Hearts Club Band", "get deep attribute from model");
     equal(song.get('album.release.year'), "1987", "get deep attribute from object");
 });
+
+test("get root attributes with wildcard", function() {
+    equal(song.get('*')[0], "Lucy In The Sky With Diamonds", "title is in attributes");
+    equal(song.get('*')[1], song.get('album'), "title is in attributes");
+});
+
+test("get album attributes with wildcard", function() {
+    equal(song.get('album.*')[0], "Sgt. Pepper's Lonely Hearts Club Band", "album name is in attributes");
+    equal(song.get('album.*')[1].year, "1987", "year is in attributes");
+});
+
+test("get year with wildcard", function() {
+    equal(song.get('album.release.*')[0], "1987", "get 'unknown' attribute from release with wildcard");
+});
